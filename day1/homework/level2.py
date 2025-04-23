37c7f9267cdc99b9d0b07d0e40622a396074eb9a
BASA0406 <bbirtvel@gmail.com>
2025-04-24 00:03:29 +0400

    GOA

---
+++ day1/homework/level2.py
@@ -0,0 +1,18 @@
+# Oto Tugushi
+
+# თემური ინაიშვილი
+
+# Saba mindiashvili
+
+# Vasili Khachiperadze
+
+# დემე სუხიტა
+
+# Dato Khvistani
+
+# Aalex Kamashidze
+
+# Alexs Gakharia
+
+toy = "gun"
+print(toy)

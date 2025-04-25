4a027c31109dd7f60cc8b7dca246dc39a58fbe71
BASA0406 <bbirtvel@gmail.com>
2025-04-25 21:26:19 +0400

    GOA

---
+++ day2/homework/homework.py
@@ -0,0 +1,33 @@
+print("Task 1")
+
+      
+Animals1 = "datvi"
+print(Animals1)
+
+Animals2 = "kata"
+print(Animals2)
+
+Animals3 = "dzagli"
+print(Animals3)
+
+
+
+Numbers1 = 100
+print(Numbers1)
+
+Numbers2 = 200
+print(Numbers2)
+
+Numbers3 = 300
+print(Numbers3)
+
+
+
+Decimals1 = 10.5
+print(Decimals1)
+
+Decimals2 = 15.4
+print(Decimals2)
+
+Decimals3 = 5.9
+print(Decimals3)
---
+++ day2/homework/homrwork.py
@@ -0,0 +1,49 @@
+print("Task 2")
+
+
+Mathematics1 = (10 + 10)
+print(Mathematics1)
+
+Mathematics2 = (15 + 5)
+print(Mathematics2)
+
+Mathematics3 = (35 - 15)
+print(Mathematics3)
+
+Mathematics4 = (50 - 30 )
+print(Mathematics4)
+
+Mathematics5 = (5 * 4)
+print(Mathematics5)
+
+Mathematics6 = (10 * 2)
+print(Mathematics6)
+
+Mathematics7 = (40 / 2)
+print(Mathematics7)
+
+Mathematics8 = (100 / 5)
+print(Mathematics8)
+
+
+
+print("Task 3")
+
+
+
+Action1 = "memivdivar"
+
+Action2 = "magaziashi"
+
+Action3 = "kvercxis"
+
+Action4 = "sayidlad"
+print(Action1 + Action2 + Action3 + Action4)
+
+
+
+
+
+
+
+

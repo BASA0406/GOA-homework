0a03a40daf8fa87691c5e599856f9d2b8884145d
BASA0406 <bbirtvel@gmail.com>
2025-05-14 00:12:01 +0400

    GOA

---
+++ day6/classwork/claswork.py
@@ -0,0 +1,31 @@
+# რას გამოიტანს ეს კოდი?
+
+x = 10
+print(x > 5 and x < 15)
+# ეს კოდი გამოიტანს - true
+
+
+age = 17
+print(age < 18 or age > 65)
+#ეს კოდი გამოიტანს - true
+
+
+
+password = "admin"
+print(password != "1234" and password != "password")
+#ეს კოდი გამოიტანს - true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

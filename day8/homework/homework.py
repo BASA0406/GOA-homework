85db7e3be86752d680be2c5869715366cd6c94ba
BASA0406 <bbirtvel@gmail.com>
2025-05-20 18:27:48 +0400

    GOA

---
+++ day8/homework/homework.py
@@ -0,0 +1,16 @@
+
+for i in range(5):
+    print("hello world")
+
+for i in range(10):
+    print("khinkali")
+
+for i in range(21):
+    print("khinkali")
+
+for i in range(101):
+    print(i)
+
+# algorithms - ნაბიჯ ნაბიჯ ინსტრუქცია რაღაც დავალების შესასრულებლად
+
+# Flowcharts - ალგორითმის ნახატად წარმოდგენა

e2d740101b16d9dc26e0904aa709e23c1b667773
BASA0406 <bbirtvel@gmail.com>
2025-05-22 16:56:32 +0400

    GOA

---
+++ day9/classwork/classwork.py
@@ -0,0 +1,17 @@
+
+i = 1
+
+while i <= 10:
+     print(i)
+     i = i + 1
+
+i = 10 
+
+while i >= 1:
+    print(i)
+    i = i - 1
+
+while True:
+     print("saqartvelo")
+
+# while loop - ამეორებს ერთიდაიგივეს სანამ რაიმე ბრძანება არ გააჩერებს
